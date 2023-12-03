8b0051e50995da4300de7d09e67ddc7c8c70b829
Adam Vogt <vogt.adam@gmail.com>
2023-12-02 20:11:37 -0500

    oled displays PPB

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 
 - AGS02MA
   - [x] reads 32ppb inside and >100ppb when above isopropyl
+  - [x] use old i2c.h instead to match espressif SSD1306 driver
   - [ ] redo wiring
-  - [ ] use i2c.h instead to match espressif SSD1306 driver
-- [ ] SSD1306 draw PPB graph, setpoints, or RPM
+- SSD1306 need layout for the following:
+   - [x] PPB digits
+   - [ ] graph PPB
+   - [ ] setpoint digits
+   - [ ] RPM digits
 - [ ] XL4015 to 3 pin fan
   - [ ] yellow tachometer wire GPIO interrupt (no PCNT available)
   - [ ] PID: LEDC changes the voltage at the trim pot voltage divider
-- [ ] EC11 to adjust PPB setpoint 
+- [ ] EC11 to adjust PPB setpoint
--- main/main.c
+++ main/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "driver/i2c_master.h"
+#include "driver/i2c.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
-// #include "ssd1306.h" // https://components.espressif.com/components/espressif/ssd1306
+#include "ssd1306.h" // https://components.espressif.com/components/espressif/ssd1306
 
 #define I2C_MASTER_NUM I2C_NUM_0
 
@@ -26,130 +26,47 @@ uint8_t crc8(uint8_t *data, uint8_t len) {
         return crc;
 }
 
-static i2c_master_dev_handle_t ags_handle;
-// static ssd1306_handle_t ssd_handle;
-
-
-esp_err_t ags_rread(uint8_t reg, uint32_t *r) {
-        uint8_t data[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
-        while(true) {
-                esp_err_t err;
-                if (reg == 0x00) {
-                        err = i2c_master_receive(ags_handle, data, 5, 200);
-                } else err = i2c_master_transmit_receive(ags_handle, &reg, 1, data, 5, 100);
-                // check crc
-                bool ok = crc8(data, 4) == data[4];
-                bool rdy = data[0] & 0x1;
-                if (!ok) {
-                        ESP_LOGE("AGS02MA", "CRC error"); 
-                        printf("data: %02x %02x %02x %02x %02x\n", data[0], data[1], data[2], data[3], data[4]);
-                        printf("crc: %02x\n", crc8(data, 4));
-                        vTaskDelay(100);
-                        continue;
-                }
-                if (reg == 0 && !rdy) {
-                        ESP_LOGI("AGS02MA", "Not ready, not retrying...");
-                        // continue;
-                }
-                if (err == ESP_ERR_INVALID_ARG) {
-                        ESP_LOGE("AGS02MA", "Invalid I2C arg");
-                        return err;
-                }
-                if (err == ESP_ERR_TIMEOUT) {
-                        ESP_LOGI("AGS02MA", "I2C timeout");
-                        continue;
-                }
-                ESP_LOGI("AGS02MA", "writing to *r");
-                *r = (data[0]<<24) | (data[1]<<16) | (data[2]<<8) | data[3];
-                return ESP_OK;
-        }
-}
-
-// reset zero point
-esp_err_t ags_reset() {
-        uint8_t data[7] = {0x01, 0x00, 0x0C, 0xFF, 0xF3, 0x00};
-        data[6] = crc8(data+2, 4);
-        return i2c_master_transmit(ags_handle, data, 7, 100);
-}
-
-esp_err_t ags_set_addr(uint8_t addr) {
-        uint8_t rdda = 0;
-        // rdda is addr bitwise-reversed
-        for (int i=0; i<7; i++) {
-                rdda |= ((addr>>i) & 0x1) << i;
-        }
-        uint8_t data[6] = {0x21, addr, rdda, addr, rdda, 0x00};
-        data[5] = crc8(data+1, 4);
-        return i2c_master_transmit(ags_handle, data, 6, 100);
-}
-
-
-esp_err_t ags_read_version(uint32_t *r) {
-        esp_err_t err = ags_rread(0x11, r);
-        *r = 0xFF & *r; // keep lowest 8 bits, the rest is reserved might not be zero
-        return err;
-}
-
-esp_err_t ags_read_hectohm(uint32_t *r) {
-        return ags_rread(0x20, r);
-}
-
-esp_err_t ags_read(uint32_t *r) { 
-        esp_err_t err = ags_rread(0x00, r); 
-        uint8_t unit = (*r >> 25) & 0x7;
-        if (unit != 0) ESP_LOGE("AGS02MA", "Unit is not PPB CH[2:0] = %d", unit);
-        // status was ok, and now unit is PPB logged
-        *r <<= 8;
-        *r >>= 8;
-        return err;
-}
-
-
-
+static ssd1306_handle_t ssd1306_dev;
 
 void app_main(void)
 {
-        // initialize i2c at 30 kHz the maximum for AGS02MA
-        // also for SSD1306
-        i2c_master_bus_config_t conf = {
-                .i2c_port = I2C_MASTER_NUM,
-                .scl_io_num = 2,
+        
+        i2c_config_t conf = {
+                .mode = I2C_MODE_MASTER,
                 .sda_io_num = 3,
-                .clk_source = I2C_CLK_SRC_DEFAULT, // or I2C_CLK_SRC_RC_FAST
-                .glitch_ignore_cnt = 7,
-                .flags.enable_internal_pullup = true,
+                .sda_pullup_en = GPIO_PULLUP_ENABLE,
+                .scl_io_num = 2,
+                .scl_pullup_en = GPIO_PULLUP_ENABLE,
+                .master.clk_speed = 30000,
+                .clk_flags = I2C_SCLK_SRC_FLAG_FOR_NOMAL,
         };
-        i2c_device_config_t dev_conf = {
-                .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-                .device_address = AGS_ADDR,
-                .scl_speed_hz = 30000,
-        };
-        i2c_master_bus_handle_t bus_handle;
-        i2c_new_master_bus(&conf, &bus_handle);
-        i2c_master_bus_add_device(bus_handle, &dev_conf, &ags_handle);
+        i2c_param_config(I2C_MASTER_NUM, &conf);
+        i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
 
+        ssd1306_dev = ssd1306_create(I2C_MASTER_NUM, SSD1306_I2C_ADDRESS);
+        ssd1306_refresh_gram(ssd1306_dev);
+        ssd1306_clear_screen(ssd1306_dev, 0x00);
 
-        // ssd_handle = ssd1306_create(I2C_MASTER_NUM, SSD1306_I2C_ADDRESS);
-        // ssd1306_refresh_gram(ssd_handle);
-        // ssd1306_clear_screen(ssd_handle, 0x00);
-        // char data_str[10] = {0};
-        // sprintf(data_str, "C STR");
-        // ssd1306_draw_string(ssd_handle, 70, 16, (const uint8_t *)data_str, 16, 1);
-        // ssd1306_refresh_gram(ssd_handle);
-
-
+        char data_str[10] = {0};
         while(true) {
+                // read PPB from AGS
+                uint8_t ags_dat[5];
+                i2c_master_read_from_device(I2C_MASTER_NUM,
+                                AGS_ADDR,
+                                ags_dat, 5, 100);
+                // check CRC
+                if (crc8(ags_dat, 4) != ags_dat[4]) {
+                        ESP_LOGI("AGS02MA", "CRC error\n");
+                        continue;
+                }
+                uint32_t ppb = (ags_dat[1] << 16) | (ags_dat[2] << 8) | ags_dat[3];
+                // report ready?
 
-                uint32_t ppb, hectohm, version;
-                ESP_ERROR_CHECK(ags_read(&ppb));
-                // ESP_ERROR_CHECK(ags_read_hectohm(&hectohm));
-                // ESP_ERROR_CHECK(ags_read_version(&version));
+                // display on OLED
+                sprintf(data_str, "%ld", ppb);
+                ssd1306_draw_string(ssd1306_dev, 70, 16, (const uint8_t *)data_str, 16, 1);
+                ssd1306_refresh_gram(ssd1306_dev);
 
-                // printf("PPB: %ld, Hectohm: %ld, Version: %ld\n", (long)ppb, (long)hectohm, (long)version);
-                printf("PPB: %ld\n", (long)ppb);
-                vTaskDelay(2000 / portTICK_PERIOD_MS);
-
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
         }
-
-
 }
